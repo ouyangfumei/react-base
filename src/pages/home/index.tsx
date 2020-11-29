06a867b10c89bc124fc9316c6749e649e87b1516
ouyangfumei <ouyangfumei@mbcloud.com>
2020-11-29 16:41:34 +0800

    将原来的redux使用方式改为react-redux

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,87 +4,100 @@ import { Input,Button } from 'antd';
 import store from '../../store';
 import * as ActionCreators from '../../store/action/actionCreators';
 import axios from 'axios';
+import {connect} from 'react-redux';
 
 // redux的应用
 // 获取：store.getState()；
 // 修改 订阅storeChange subscribe
 interface IProps {
+    inputValue: string,
     name: string;
+    list:Array<string>;
+    changeInputValue:any;
+    getData:any;
+    addBtn:any;
+    delBtn:any;
 }
 interface IState {
     inputValue: string,
     list:Array<string>
 }
+// state
 class Home extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props);
-        const storeData = store.getState();
-        this.state = {
-            inputValue:storeData.inputValue,
-            list:storeData.list
-        };
-        this.changeInputValue = this.changeInputValue.bind(this);
+        // 未使用react-redux
+        // const storeData = store.getState();
+        // const inputValue = storeData.inputValue;
+        // const list = storeData.list;
+        // 使用react-redux
+        // const {inputValue,list} = this.props;
+        // this.state = {
+        //     inputValue,
+        //     list
+        // };
         // ---修改store 订阅Redux的状态
-        this.storeChange = this.storeChange.bind(this);
-        store.subscribe(this.storeChange);
+        // this.storeChange = this.storeChange.bind(this);
+        // store.subscribe(this.storeChange);
         // ---
-        this.addBtn = this.addBtn.bind(this);
-        this.delBtn = this.delBtn.bind(this);
     } 
     componentDidMount(){  
-        this.getData();
+        this.props.getData();
     }
-    getData(){
-        // 未使用中间件thunk的写法
-        // axios.get('/mock') 
-        //     .then(res=>{
-        //     console.log(res.data.userinfo);
-        //     const resData = res.data.userinfo;
-        //     const action = ActionCreators.getListAction(resData);
-        //     store.dispatch(action);
-        // })
-        // 修改后，使用中间件的写法
-        const action = ActionCreators.getList();
-        store.dispatch(action);
-    }
-    changeInputValue(e: React.ChangeEvent<HTMLInputElement>){
-        const action = ActionCreators.changeInputAction(e.target.value);
-        store.dispatch(action);
-    }  
-    storeChange(){
-        console.log(store.getState());
-        const storeData = store.getState();
-        this.setState({
-            inputValue:storeData.inputValue,
-            list:storeData.list
-        })
-    }   
-    addBtn(){
-        const action = ActionCreators.addItemAction();
-        store.dispatch(action);
-    }
-    delBtn(){
-        const action = ActionCreators.deleteItemAction(0);
-        store.dispatch(action);
-    }
+    // storeChange(){
+    //     console.log(store.getState());
+    //     const storeData = store.getState();
+    //     this.setState({
+    //         inputValue:storeData.inputValue,
+    //         list:storeData.list
+    //     })
+    // }   
     render() { 
-        const {list} = this.state;
+        const {inputValue,list,changeInputValue,delBtn,addBtn} = this.props;
         return (  
             <div className="home">
                 <p>列表如下，增删改查</p>
                <div className="input-center">
-                   <Input value={this.state.inputValue} onChange={this.changeInputValue}/>
+                   <Input value={inputValue} onChange={changeInputValue}/>
                    <Button 
                     type="primary"
-                    onClick={this.addBtn}>
+                    onClick={addBtn}>
                     增加</Button>
                 </div>
                 {list.length&&list.map((item,index)=>
-                    <p key={index}><span>{item}&nbsp;&nbsp;&nbsp;&nbsp;</span><span className="del" onClick={this.delBtn}>删除</span></p>
+                    <p key={index}><span>{item}&nbsp;&nbsp;&nbsp;&nbsp;</span><span className="del" onClick={delBtn}>删除</span></p>
                 )}
             </div> 
        );
     }
 }
+// export default Home;
 
-export default Home;
+// react-redux的使用
+const stateToProps = (state:IState)=>{
+    return{
+        inputValue:state.inputValue,
+        list:state.list
+    }
+}
+const dispatchToProps = ()=>{
+    return {
+        changeInputValue(e: React.ChangeEvent<HTMLInputElement>){
+            const action = ActionCreators.changeInputAction(e.target.value);
+            store.dispatch(action);
+        },
+        getData(){
+            const action = ActionCreators.getList();
+            store.dispatch(action);
+        },
+        addBtn(){
+            const action = ActionCreators.addItemAction();
+            store.dispatch(action);
+        },
+        delBtn(){
+            const action = ActionCreators.deleteItemAction(0);
+            store.dispatch(action);
+        }
+    }
+}
+export default connect(stateToProps,dispatchToProps)(Home);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import {HashRouter,Route,Link,Switch,Redirect} from 'react-router-dom';
 import BaseRoutes from './base';
+import { Provider } from 'react-redux';
+import store from '../store';
 
-
-// 后期加入radux的使用todo
-// import { Provider } from 'react-redux';
-// import rootReducer from '@/redux/index';
-// import configStore from '@/redux/store';
-// const store = configStore(rootReducer);
 /**
  * 1.react-router-dom使用
  * HashRouter:如有多个子元素，则需要一个父容器将多个子元素全部包裹，否则报错
@@ -16,25 +12,29 @@ import BaseRoutes from './base';
  * Redirect：重定向
  * 2.redux的结合
  * Provider：provider包裹在根组件外层，使所有的子组件都可以拿到state
+ * 
+ * 
 */
 const defaultRoute = '/'
 export default class AppRouter extends React.Component{
     render() {
         return(
-            <HashRouter>
-                <ul>
-                    <li> <Link to="/">首页home</Link> </li>
-                    <li><Link to="/list">列表</Link> </li>
-                </ul>
-                <Switch>
-                    {BaseRoutes.length&&BaseRoutes.map((route,index)=>{
-                        return(
-                            <Route key={index} path={route.path} exact component={route.component} />
-                        )
-                    })}
-                    <Redirect to={defaultRoute}></Redirect>
-                </Switch>
-            </HashRouter>
+            <Provider store={store}>
+                <HashRouter>
+                    <ul>
+                        <li> <Link to="/">首页home</Link> </li>
+                        <li><Link to="/list">列表</Link> </li>
+                    </ul>
+                    <Switch>
+                        {BaseRoutes.length&&BaseRoutes.map((route,index)=>{
+                            return(
+                                <Route key={index} path={route.path} exact component={route.component} />
+                            )
+                        })}
+                        <Redirect to={defaultRoute}></Redirect>
+                    </Switch>
+                </HashRouter>
+            </Provider>
        )
     }
 }
