7b84849723a84e295c1112b0955033b3c257ddcb
ouyangfumei <ouyangfumei@mbcloud.com>
2021-04-12 21:42:31 +0800

    测试gitrebase在master的提交

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@
 import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducer'
 import thunk from 'redux-thunk'
-
 // applyMiddleware使用thunk中间件
 const store = createStore(reducer, applyMiddleware(thunk))
 export default store
